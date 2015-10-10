4e8e182f85f1cdbb0bf8ba1827749996cd2a55c2
Piratica <nathan@piratica.us>
2015-10-10 13:26:59 -0400

    Create arachni.py
    Drop this into the ptf/modules/vulnerability-analysis/ directory of your ptf install to install / update the arachni web vulnerability scanner

---
+++ arachni.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#########################################
+# Installation module for arachni
+#########################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Nathan Underwood (sai nate)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="Website / webapp vulnerability scanner."
+
+# INSTALLATION TYPE 
+# OPTIONS GIT, SVN, FILE, DOWNLOAD
+INSTALL_TYPE="GIT"
+
+#LOCATION OF THE FILE OR GIT / SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/Arachni/arachni.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="arachni"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN=""
+
+#COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
