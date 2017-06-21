27fe7f07d3d4d70b95464e7a1c622520a8211dde
Piratica <nathan@piratica.us>
2017-06-21 14:15:07 -0400

    PTF Module to download / update searchsploit

---
+++ intelligence-gathering/searchsploit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#########################################
+# Installation module for searchsploit
+#########################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Nathan Underwood (sai nate)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="Download / Update SearchSploit"
+
+# INSTALLATION TYPE 
+# OPTIONS GIT, SVN, FILE, DOWNLOAD
+INSTALL_TYPE="GIT"
+
+#LOCATION OF THE FILE OR GIT / SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/offensive-security/exploit-database"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="searchsploit"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+#COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
