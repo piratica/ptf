f4a1b9a4cb44d7a47ea7a8de62caa9517b95bfb8
Piratica <nathan@piratica.us>
2017-06-21 14:25:04 -0400

    bugfix

--- intelligence-gathering/searchsploit.py
+++ intelligence-gathering/searchsploit.py
@@ -3,24 +3,27 @@
 # Installation module for searchsploit
 #########################################
 
-# AUTHOR OF MODULE NAME
-AUTHOR="Nathan Underwood (sai nate)"
+#Author NameError
+AUTHOR="Nathan Underwood (sainate)"
 
-# DESCRIPTION OF THE MODULE
-DESCRIPTION="Download / Update SearchSploit"
+DESCRIPTION="This module will download the seachsploit database"
 
-# INSTALLATION TYPE 
-# OPTIONS GIT, SVN, FILE, DOWNLOAD
 INSTALL_TYPE="GIT"
 
-#LOCATION OF THE FILE OR GIT / SVN REPOSITORY
 REPOSITORY_LOCATION="https://github.com/offensive-security/exploit-database"
 
-# WHERE DO YOU WANT TO INSTALL IT
+# X64_LOCATION="https://github.com/something_thats_x64_instead_of_x86
+
 INSTALL_LOCATION="searchsploit"
 
-# DEPENDS FOR DEBIAN INSTALLS
 DEBIAN="git"
 
-#COMMANDS TO RUN AFTER
-AFTER_COMMANDS=""
+#ARCHLINUX = "arch-module,etc"
+
+BYPASS_UPDATE="NO"
+
+#AFTER_COMMANDS="cd {INSTALL_LOCATION},ruby install-beef"
+
+#LAUNCHER="beef"
+
+#TOOL_DEPEND="modules/exploitation/metasploit"
